ac8cc854d165a4ed299c662ebd0165aa6f25abb7
Bukva Miklós <miklos.bukva@bankmonitor.hu>
2019-12-13 11:58:30 +0100

    first public version without resp.

--- src/js/main.js
+++ src/js/main.js
@@ -2,49 +2,53 @@
 /* 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=5zMcBFNZ1QzwPfh9jYjtczvCmMrH2K5Rx3GzvjJF' */
 /* https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${solChoose}&camera=fhaz&api_key=5zMcBFNZ1QzwPfh9jYjtczvCmMrH2K5Rx3GzvjJF*/
 
-const curiosityFirstTest = document.getElementById('curiosityTest');
-
+const curiosityCameraResults = document.getElementById('curiosityCameraResults');
 
 var request = new XMLHttpRequest()
 
 function getCuriosityPictures() {
     let earthDateSelect = document.getElementById('earthDateChooser').value;
     let cameraChooser = document.getElementById('cameraChooser').value;
-    console.log(earthDateSelect);
     request.open('GET', `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${earthDateSelect}&camera=${cameraChooser}&api_key=5zMcBFNZ1QzwPfh9jYjtczvCmMrH2K5Rx3GzvjJF`, true)
-
     request.onload = function () {
         var data = JSON.parse(this.response);
 
         if (request.status >= 200 && request.status < 400) {
-            console.log(data);
-            console.log(data.photos[0]);
-            console.log(data.photos.length);
-            let newInnerHTML = '';
-            
-            if(data.photos.length === 0 ) {
-                newInnerHTML = `
+            let curiosityPhotos = '';
+            let details = '';
+            let detailsDiv = document.getElementById('detailsDiv');
+
+            if (data.photos.length === 0) {
+                details = `
                 <div class="alert alert-warning" role="alert">
-  This is a warning alert—check it out!
-</div>
+                <p>Sorry, there is not any photo here. Try other cameras or days.</p>
+                <p>For example, try out my favourites:
+                <ul class="text-left">
+                <li>2014, Feb. 19. - Navigation camera</li>
+                <li>2015, Apr. 7. - Mast camera</li>
+                <li>2019, Aug. 6. - Front Hazard Avoidance Camera: <strong>first photos</strong></li>
+                </ul>
+                </div>
                 `
             }
 
             data.photos.forEach(photo => {
                 console.log(photo.img_src)
-
-                newInnerHTML += `
+                details = `<div class="alert alert-info" role="alert">
+                <p class="text-left"    >
+                Number of photos taken this sol from <i>${photo.camera.full_name}</i>: <strong>${data.photos.length}</strong><br>
+                Earth date: <strong>${photo.earth_date}</strong> <br>
+                Sol: <strong>${photo.sol}</strong> <br>
+                </p>
+                </div>
+                `;
+                curiosityPhotos += `
             
-            <img src="${photo.img_src}" class="camera-photos d-block"> 
-            <p class="text-left"    >
-            Number of photos taken this sol from <i>${photo.camera.full_name}</i>: <strong>${data.photos.length}</strong><br>
-            Earth date: <strong>${photo.earth_date}</strong> <br>
-            Sol: <strong>${photo.sol}</strong> <br>
-            </p>
-            `;
+            <img src="${photo.img_src}" class="camera-photos d-block"> <br> `;
 
             })
-            curiosityFirstTest.innerHTML = newInnerHTML;
+            curiosityCameraResults.innerHTML = curiosityPhotos;
+            detailsDiv.innerHTML = details;
         } else {
             console.log('error')
         }
@@ -52,5 +56,6 @@ function getCuriosityPictures() {
     request.send()
 }
 
+$('[data-toggle="tooltip"]').tooltip();
 
-$( "#earthDateChooser" ).datepicker({ minDate: new Date(2012, 7, 6), maxDate: new Date(2019, 9, 28) , dateFormat: 'yy-m-d', changeYear: true});
+$("#earthDateChooser").datepicker({ minDate: new Date(2012, 7, 6), maxDate: new Date(2019, 9, 28), dateFormat: 'yy-m-d', changeYear: true });
